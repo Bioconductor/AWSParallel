c110577ae31e937f31854785a6679532b54165d6
Nitesh Turaga <nitesh.turaga@gmail.com>
2018-01-05 11:16:49 -0500

    Add config files needed for SGE and starcluster, to provide a fully reproducible AMI

---
+++ inst/script/config_files/BatchJobs.R
@@ -0,0 +1,7 @@
+cluster.functions = makeClusterFunctionsSGE('/home/ubuntu/.sgetemplate/simple.tmpl')
+mail.start = "none"
+mail.done = "none"
+mail.error = "none"
+db.driver = "SQLite"
+db.options = list()
+debug = FALSE
