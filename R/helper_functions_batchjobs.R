52d81ac492367e9cd9049ca43615129599f8899d
Nitesh Turaga <nitesh.turaga@gmail.com>
2018-01-08 08:27:27 -0500

    Remove .awsDetechSubnetOnMaster as it exists in another file

--- R/helper_functions_batchjobs.R
+++ R/helper_functions_batchjobs.R
@@ -31,34 +31,12 @@
     write.ini(config, starcluster_config)
 }
 
-
-#' Detect the subnet on the AWS master instance of the cluster
+## FIXME: AMI should be listed on bioconductor.org/config.yaml
+#' This function returns the AMI ID listed on bioconductor.
 #' 
-#' @importFrom aws.ec2 describe_instances
-.awsDetectSubnetOnMaster <-
-    function()
-{
-    ## Get list of all instances on AWS account
-    instances <- describe_instances()
-    ## Get hostname of local machine code is being run on
-    hostname <- system2("hostname", stdout=TRUE)
-    hostname <- gsub("-",".", sub("ip-","", hostname))
-    subnet <- NA_character_
-    for (i in seq_along(instances)) {
-        instancesSet = instances[[i]][["instancesSet"]]
-        for (j in seq_along(instancesSet)) {
-            privateIpAddress <- instancesSet[[j]][["privateIpAddress"]]
-            if (privateIpAddress == hostname)
-                subnet <- instancesSet[[j]][["subnetId"]]
-        }
-    }
-    subnet
-}
-
-
 getStarclusterAmiId <-
     function()
 {
-
+    "ami-0454187e"
 }
 
